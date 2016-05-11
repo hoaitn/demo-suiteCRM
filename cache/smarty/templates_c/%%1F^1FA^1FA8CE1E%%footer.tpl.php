4ce5ae2887ceb594341dd78b1d7d44bb15f8ed05
Duong The <nopbongdem@gmail.com>
2016-05-11 14:42:15 +0700

    add gitignore

--- cache/smarty/templates_c/%%1F^1FA^1FA8CE1E%%footer.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.29, created on 2016-05-10 11:09:53
-         compiled from modules/Calendar/tpls/footer.tpl */ ?>
-<div class='monthFooter'>
-	<div style='float: left;'><?php echo $this->_tpl_vars['previous']; ?>
-</div><div style='float: right;'><?php echo $this->_tpl_vars['next']; ?>
-</div>
-</div>
-<br style='clear:both;'>
